98ecde113e71750e5a5f8fd5236b720013d888b5
A. Tokarev <alex.tokarew@gmail.com>
2012-11-01 07:39:31 +0400

    Initial commit

---
+++ src/com/alext/hypothec/MainActivity.java
@@ -0,0 +1,41 @@
+package com.alext.hypothec;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import com.alext.hypothec.model.HypothecCalculator;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void calculate(View button) {
+        HypothecCalculator calculator = new HypothecCalculator(
+                editTextToInt(R.id.credit_sum),
+                editTextToDoubleBigDecimal(R.id.percentage),
+                editTextToInt(R.id.credit_duration_month)
+        );
+
+        EditText monthlyPayment = (EditText) findViewById(R.id.monthly_payment);
+        monthlyPayment.setText(calculator.getMonthlyPayment().toString());
+    }
+
+    private int editTextToInt(int id) {
+        return Integer.valueOf(((EditText) findViewById(id)).getText().toString());
+    }
+
+    private BigDecimal editTextToDoubleBigDecimal(int id) {
+        return new BigDecimal(((EditText) findViewById(id)).getText().toString());
+    }
+}
